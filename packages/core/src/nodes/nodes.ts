b978833ae1e203ac26b0687d5e04e417b6f648b7
Michael Penney <michael.penney@pearlchain.net>
2020-02-26 12:48:13 +0100

    Move Start-node to Dashboards

--- packages/core/src/nodes/nodes.ts
+++ packages/core/src/nodes/nodes.ts
@@ -1,18 +1,15 @@
 import { SUB_GRAPH_NODE } from "./layout/SubGraphNode";
-import { START_NODE } from "./internal/StartNode";
 import { SUBGRAPH_START_NODE } from "./internal/SubGraphStartNode";
 import { SUBGRAPH_END_NODE } from "./internal/SubGraphEndNode";
 
 export enum NodeType {
     SUB_GRAPH='subgraph',
     SUB_GRAPH_START='subgraph-start',
-    SUB_GRAPH_END='subgraph-end',
-    START='start',
+    SUB_GRAPH_END='subgraph-end'
 }
 
 export const nodes = {
     [NodeType.SUB_GRAPH]: SUB_GRAPH_NODE,
-    [NodeType.START]: START_NODE,
     [NodeType.SUB_GRAPH_START]: SUBGRAPH_START_NODE,
     [NodeType.SUB_GRAPH_END]: SUBGRAPH_END_NODE
 };
